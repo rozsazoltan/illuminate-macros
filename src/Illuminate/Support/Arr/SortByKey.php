5ca507a86f870bd379d2e8f4a47bc36d142a5474
Rózsa Zoltán <dev@datarose.net>
2022-10-23 22:55:18 +0200

    `Arr::sortByKey` method

---
+++ src/Illuminate/Support/Arr/SortByKey.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Datarose\LaravelMacros\Illuminate\Support\Arr;
+
+/**
+ * Sort elements of array by key (or column)
+ *
+ * @param array $array
+ * @param string|null $column
+ * @param bool $descending
+ *
+ * @mixin \Illuminate\Support\Arr
+ *
+ * @return array
+ */
+class SortByKey
+{
+  public function __invoke()
+  {
+    return function ($array, $column = null, $descending = false) {
+      return collect($array)->sortBy(function ($value, $key) use ($column) {
+        return is_null($column) ? $key : $value[$column];
+      }, \SORT_REGULAR, $descending)->toArray();
+    };
+  }
+}

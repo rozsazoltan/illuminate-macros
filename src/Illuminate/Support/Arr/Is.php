50bb943b44255079c5259be53b67223c468c1532
Rózsa Zoltán <dev@datarose.net>
2022-10-23 22:54:44 +0200

    `Arr::is` method

---
+++ src/Illuminate/Support/Arr/Is.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Datarose\LaravelMacros\Illuminate\Support\Arr;
+
+/**
+ * Variable is array or not
+ *
+ * @param mixed $variable
+ *
+ * @mixin \Illuminate\Support\Arr
+ *
+ * @return bool
+ */
+class Is
+{
+  public function __invoke()
+  {
+    return function (mixed $variable): bool {
+      return is_array($variable);
+    };
+  }
+}

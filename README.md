a53160d912e57e0c786aae0f30a8fb26b7d809ee
Rózsa Zoltán <dev@datarose.net>
2022-10-24 10:36:43 +0200

    Rename package to illuminate-macros
    - change namespaces (IlluminateMacro)
    - change name
    - update README

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 ##### Datarose Production
 
-## laravel-macros
+## illuminate-macros
 
 #### Information
 
 [![Go to Github profile](https://img.shields.io/static/v1?label=datarose&message=production&color=2ea44f&logo=github)](https://github.com/rozsazoltan)
-[![@datarose/laravel-macros - v1.x](https://img.shields.io/static/v1?label=@datarose/laravel-macros&message=v1.x&color=e31d65)](https://github.com/rozsazoltan/laravel-macros/blob/dev-master/README.md)
-[![GNU General Public License 3.0 license](https://img.shields.io/static/v1?label=License&message=GNU-GPL-3.0&color=2ea44f)](https://github.com/rozsazoltan/laravel-macros/blob/dev-master/LICENSE.md)
-[![Views](https://komarev.com/ghpvc/?username=rozsazoltan-laravel-macros&label=Views)](https://github.com/rozsazoltan/laravel-macros/blob/dev-master/LICENSE.md)
+[![@datarose/illuminate-macros - v1.x](https://img.shields.io/static/v1?label=@datarose/illuminate-macros&message=v1.x&color=e31d65)](https://github.com/rozsazoltan/illuminate-macros/blob/dev-master/README.md)
+[![GNU General Public License 3.0 license](https://img.shields.io/static/v1?label=License&message=GNU-GPL-3.0&color=2ea44f)](https://github.com/rozsazoltan/illuminate-macros/blob/dev-master/LICENSE.md)
+[![Views](https://komarev.com/ghpvc/?username=rozsazoltan-illuminate-macros&label=Views)](https://github.com/rozsazoltan/illuminate-macros/blob/dev-master/LICENSE.md)
 
 <br><br>
 
@@ -16,35 +16,110 @@
 <details>
   <summary>Documentation links</summary>
 
+  <br>
+
   - [Getting Started](#getting-started)
     - [Installation](#installation)
     - [Usage](#usage)
+  - [Helper Functions](#helper-functions)
+    - [Illuminate\Support\Arr](#arrclass)
+    - [Illuminate\Support\Str](#strclass)
+  - [Collection Functions](#collection-functions)
+    - [Illuminate\Support\Collection](#collectionclass)
+    - [Illuminate\Support\LazyCollection](#lazycollectionclass)
+  - [Routing Functions](#routing-functions)
+    - [Illuminate\Routing\Route (alias Get Route Details)](#routeclass)
   - [Appendix](#appendix)
     - [License](#license)
-    - [Special Thanks](#special-thanks)
 </details>
 
 >**Note:** Documentation links will working after open to Documentation content.
 
+<br>
+
 <details>
   <summary>Documentation content</summary>
 
-  ### Getting Started
+  <br>
 
-  #### Installation
+  ### # Getting Started
+
+  #### | Installation
 
   Require:
   - PHP 8.x or heigher
-  - Laravel 8.x or heigher
+  - Laravel 9.x or heigher
 
   ```sh
     # Install
-    composer install datarose/laravel-macros
+    composer install datarose/illuminate-macros
   ```
 
-  #### Usage
+  #### | Usage
 
-  ...
+  There is nothing else to do. Just use it with default Laravel Framework.<br>
+  ex.: In package can found `Collection::rangeByKey` method. How can use it?
+
+  ```php
+  use Illuminate\Support\Collection;
+
+  $collection = Collection::rangeByKey(-2, 2, fn ($key) => $key + 2);
+
+  /*
+   $collection result:
+   [
+    -2 => 0,
+    -1 => 1,
+     0 => 2,
+     1 => 3,
+     2 => 4
+   ]
+  */
+  ```
+
+  <br><br>
+
+  ### # Helper Functions
+
+  #### | Arr::class
+
+  - `In`
+  - `Is`
+  - `Merge`
+  - `Similar`
+  - `SortByKey`
+  - `SortByKeyDesc`
+
+  #### | Str::class
+
+  - `Equals`
+  - `HasEquals`
+  - `Explode`
+  - `ToStrId`
+  - `UniqueString`
+  - `CleanString`
+  - `GetModelName`
+  - `GetLicenses`
+
+  <br><br>
+
+  ### # Collection Functions
+
+  #### | Collection::class
+
+  - `RangeByKey`
+
+  #### | LazyCollection::class
+
+  - `RangeByKey`
+
+  <br><br>
+
+  ### # Routing Functions
+
+  #### | Route::class
+
+  - `GetGroupNames`
 
 </details>
 
@@ -52,7 +127,7 @@
 
 ## Appendix
 
-### License
-#### @datarose/laravel-macros
-Licensed under the [GNU General Public License 3.0](https://github.com/rozsazoltan/laravel-macros/blob/dev-master/LICENSE.md) for compatible open source projects and non-commercial use.<br>
+### # License
+#### @datarose/illuminate-macros
+Licensed under the [GNU General Public License 3.0](https://github.com/rozsazoltan/illuminate-macros/blob/dev-master/LICENSE.md) for compatible open source projects and non-commercial use.<br>
 © 2022 Rózsa Zoltán
---
+++ src/Illuminate/Foundation/IlluminateMacroServiceProvider.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Datarose;
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\ServiceProvider;
+
+class IlluminateMacroServiceProvider extends ServiceProvider
+{
+  public function register()
+  {
+    Collection::make($this->macros())
+      ->each(fn ($macros, $class) => Collection::make($macros)
+        ->reject(fn ($name, $macro) => $class::hasMacro($macro))
+        ->each(fn ($name, $macro) => $class::macro($macro, app($name)()))
+      );
+  }
+
+  private function macros(): array
+  {
+    return [
+      \Illuminate\Routing\Route::class => [
+        'getGroupNames' => \IlluminateMacro\Routing\Route\GetGroupNames::class,
+      ],
+
+      \Illuminate\Support\Arr::class => [
+        'in' => \IlluminateMacro\Support\Arr\In::class,
+        'is' => \IlluminateMacro\Support\Arr\Is::class,
+        'merge' => \IlluminateMacro\Support\Arr\Merge::class,
+        'similar' => \IlluminateMacro\Support\Arr\Similar::class,
+        'sortByKey' => \IlluminateMacro\Support\Arr\SortByKey::class,
+        'sortByKeyDesc' => \IlluminateMacro\Support\Arr\SortByKeyDesc::class,
+      ],
+      \Illuminate\Support\Str::class => [
+        'equals' => \IlluminateMacro\Support\Str\Equals::class,
+        'explode' => \IlluminateMacro\Support\Str\Explode::class,
+        'uniqueString' => \IlluminateMacro\Support\Str\UniqueString::class,
+        'cleanString' => \IlluminateMacro\Support\Str\CleanString::class,
+        'toStrId' => \IlluminateMacro\Support\Str\ToStrId::class,
+        'getModelName' => \IlluminateMacro\Support\Str\GetModelName::class,
+        'getLicenses' => \IlluminateMacro\Support\Str\GetLicenses::class,
+      ],
+
+      \Illuminate\Support\Collection::class => [
+        'after' => \IlluminateMacro\Support\Collection\After::class,
+        'at' => \IlluminateMacro\Support\Collection\At::class,
+        'before' => \IlluminateMacro\Support\Collection\Before::class,
+        'chunkBy' => \IlluminateMacro\Support\Collection\ChunkBy::class,
+        'collectBy' => \IlluminateMacro\Support\Collection\CollectBy::class,
+        'containsAll' => \IlluminateMacro\Support\Collection\ContainsAll::class,
+        'containsAny' => \IlluminateMacro\Support\Collection\ContainsAny::class,
+        'eachCons' => \IlluminateMacro\Support\Collection\EachCons::class,
+        'eighth' => \IlluminateMacro\Support\Collection\Eighth::class,
+        'extract' => \IlluminateMacro\Support\Collection\Extract::class,
+        'fifth' => \IlluminateMacro\Support\Collection\Fifth::class,
+        'filterMap' => \IlluminateMacro\Support\Collection\FilterMap::class,
+        'firstOrFail' => \IlluminateMacro\Support\Collection\FirstOrFail::class,
+        'firstOrPush' => \IlluminateMacro\Support\Collection\FirstOrPush::class,
+        'fourth' => \IlluminateMacro\Support\Collection\Fourth::class,
+        'fromPairs' => \IlluminateMacro\Support\Collection\FromPairs::class,
+        'getNth' => \IlluminateMacro\Support\Collection\GetNth::class,
+        'glob' => \IlluminateMacro\Support\Collection\Glob::class,
+        'groupByModel' => \IlluminateMacro\Support\Collection\GroupByModel::class,
+        'head' => \IlluminateMacro\Support\Collection\Head::class,
+        'if' => \IlluminateMacro\Support\Collection\IfMacro::class,
+        'ifAny' => \IlluminateMacro\Support\Collection\IfAny::class,
+        'ifEmpty' => \IlluminateMacro\Support\Collection\IfEmpty::class,
+        'insertAfter' => \IlluminateMacro\Support\Collection\InsertAfter::class,
+        'insertAfterKey' => \IlluminateMacro\Support\Collection\InsertAfterKey::class,
+        'insertAt' => \IlluminateMacro\Support\Collection\InsertAt::class,
+        'insertBefore' => \IlluminateMacro\Support\Collection\InsertBefore::class,
+        'insertBeforeKey' => \IlluminateMacro\Support\Collection\InsertBeforeKey::class,
+        'ninth' => \IlluminateMacro\Support\Collection\Ninth::class,
+        'none' => \IlluminateMacro\Support\Collection\None::class,
+        'paginate' => \IlluminateMacro\Support\Collection\Paginate::class,
+        'parallelMap' => \IlluminateMacro\Support\Collection\ParallelMap::class,
+        'path' => \IlluminateMacro\Support\Collection\Path::class,
+        'pluckMany' => \IlluminateMacro\Support\Collection\PluckMany::class,
+        'pluckToArray' => \IlluminateMacro\Support\Collection\PluckToArray::class,
+        'prioritize' => \IlluminateMacro\Support\Collection\Prioritize::class,
+        'rangeByKey' => \IlluminateMacro\Support\Collection\RangeByKey::class,
+        'recursive' => \IlluminateMacro\Support\Collection\Recursive::class,
+        'rotate' => \IlluminateMacro\Support\Collection\Rotate::class,
+        'second' => \IlluminateMacro\Support\Collection\Second::class,
+        'sectionBy' => \IlluminateMacro\Support\Collection\SectionBy::class,
+        'seventh' => \IlluminateMacro\Support\Collection\Seventh::class,
+        'simplePaginate' => \IlluminateMacro\Support\Collection\SimplePaginate::class,
+        'sixth' => \IlluminateMacro\Support\Collection\Sixth::class,
+        'sliceBefore' => \IlluminateMacro\Support\Collection\SliceBefore::class,
+        'tail' => \IlluminateMacro\Support\Collection\Tail::class,
+        'tenth' => \IlluminateMacro\Support\Collection\Tenth::class,
+        'third' => \IlluminateMacro\Support\Collection\Third::class,
+        'toPairs' => \IlluminateMacro\Support\Collection\ToPairs::class,
+        'transpose' => \IlluminateMacro\Support\Collection\Transpose::class,
+        'try' => \IlluminateMacro\Support\Collection\TryCatch::class,
+        'validate' => \IlluminateMacro\Support\Collection\Validate::class,
+        'weightedRandom' => \IlluminateMacro\Support\Collection\WeightedRandom::class,
+        'withSize' => \IlluminateMacro\Support\Collection\WithSize::class,
+      ],
+      \Illuminate\Support\LazyCollection::class => [
+        'rangeByKey' => \IlluminateMacro\Support\LazyCollection\RangeByKey::class,
+      ],
+    ];
+  }
+}
